28777cdddfef315312b295a8546db2aae6dd5bff
Vishal Mishra <vishalmishra1249@gmail.com>
2025-10-25 00:59:32 +0530

    Created login form

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,9 +1,23 @@
-import React from 'react'
+import Header from "./Header";
 
 const Login = () => {
   return (
-    <div>Login</div>
-  )
-}
+    <div>
+      <Header />
+      <div className="absolute">
+        <img
+          src="https://assets.nflxext.com/ffe/siteui/vlv3/9ba9f0e2-b246-47f4-bd1f-3e84c23a5db8/web/IN-en-20251020-TRIFECTA-perspective_d6da84e9-6145-4b1e-bb51-e402c966a045_large.jpg"
+          alt="logo"
+        />
+      </div>
 
-export default Login
+      <form className="relative p-12 bg-black">
+        <input type="text" placeholder="Email Address" className="p-2 m-2"/>
+        <input type="password" placeholder="Password" className="p-2 m-2"/>
+        <button className="p-4 m-4">Sigin In</button>
+      </form>
+    </div>
+  );
+};
+
+export default Login;
