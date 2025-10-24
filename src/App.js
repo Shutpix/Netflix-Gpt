2ef65dd7c6ae47386f78c5a685193b256c48724f
Vishal Mishra <vishalmishra1249@gmail.com>
2025-10-24 23:54:14 +0530

    Added tailwind css

--- src/App.js
+++ src/App.js
@@ -2,8 +2,9 @@ import React from 'react'
 
 const App = () => {
   return (
-    <div>Hellow world</div>
-  )
+  <h1 className="text-3xl font-bold underline">
+      Hello world!
+    </h1>  )
 }
 
 export default App
