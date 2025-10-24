811a476a931232ec6170820cee1d048bec22a2d5
Vishal Mishra <vishalmishra1249@gmail.com>
2025-10-25 00:18:35 +0530

    added components as well as reac-router-dom

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
-import React from 'react'
+import Body from './components/Body'
 
 const App = () => {
   return (
-  <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>  )
+    <Body/>
+   )
 }
 
 export default App
