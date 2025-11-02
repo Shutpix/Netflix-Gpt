c3256035d2bf2f33c7d4bb8eadbffd1722cc1477
Vishal Mishra <vishalmishra1249@gmail.com>
2025-11-02 17:35:20 +0530

    added upcoming, toprated and popular section

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -1,13 +1,16 @@
-import React from 'react'
-import { IMG_CDN_URL } from '../utils/constants'
+import React from "react";
+import { IMG_CDN_URL } from "../utils/constants";
 
-const MovieCard = ({posterPath}) => {
+const MovieCard = ({ posterPath }) => {
   return (
-    <div className='w-48 pr-4 '>
-        <img alt= "Movie card"
-        src={IMG_CDN_URL + posterPath}/>
+    <div className="w-48 pr-4 transform transition-transform duration-300 ease-in-out hover:scale-125 hover:z-20 hover:shadow-2xl">
+      <img
+        alt="Movie card"
+        src={IMG_CDN_URL + posterPath}
+        className="rounded-md w-full"
+      />
     </div>
-  )
-}
+  );
+};
 
-export default MovieCard
+export default MovieCard;
--- src/components/SecondaryContainer.js
+++ src/components/SecondaryContainer.js
@@ -8,7 +8,9 @@ const SecondaryContainer = () => {
       <div className=" bg-black">
         <div className="pl-12 -mt-52 relative z-20">
           <MovieList title="Now Playing" movies={movies?.nowPlayingMovies} />
+          <MovieList title="Upcoming" movies={movies?.upcomingMovies} />
           <MovieList title="Popular" movies={movies?.popularMovies} />
+          <MovieList title="Top Rated" movies={movies?.topRatedMovies} />
           <MovieList title="Horror" movies={movies?.nowPlayingMovies} />
           <MovieList title="Romance" movies={movies?.nowPlayingMovies} />
         </div>
