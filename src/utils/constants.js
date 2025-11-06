1750256892994deaf97ad393acf2d068004ca346
Vishal Mishra <vishalmishra1249@gmail.com>
2025-11-06 22:20:17 +0530

    Code cleaning

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -5,11 +5,13 @@ export const PHOTO_URL = "https://occ-0-3365-2186.1.nflxso.net/dnm/api/v6/vN7bi_
 export const NETFLIX_BG = "https://assets.nflxext.com/ffe/siteui/vlv3/9ba9f0e2-b246-47f4-bd1f-3e84c23a5db8/web/IN-en-20251020-TRIFECTA-perspective_d6da84e9-6145-4b1e-bb51-e402c966a045_large.jpg";
 
 export const API_OPTIONS = {
-  method: 'GET',
+  method: "GET",
+  method: "GET",
   headers: {
-    accept: 'application/json',
-    Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJhODNmNDY1Y2JkNzczMzBlZmEyMzJjNGE5YzdkZWM4OSIsIm5iZiI6MTc2MTQ5ODcyNy4zNTUsInN1YiI6IjY4ZmU1NjY3ZmZlODY4OGZiNDkyNzA4ZCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.pvLV4Br_7Z02bY9sNRwqOaG2owY3yhrnGdma2D3zJVE'
-  }
+    accept: "application/json",
+    Authorization:
+      "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJhODNmNDY1Y2JkNzczMzBlZmEyMzJjNGE5YzdkZWM4OSIsIm5iZiI6MTc2MTQ5ODcyNy4zNTUsInN1YiI6IjY4ZmU1NjY3ZmZlODY4OGZiNDkyNzA4ZCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.pvLV4Br_7Z02bY9sNRwqOaG2owY3yhrnGdma2D3zJVE",
+  },
 };
 
 export const IMG_CDN_URL = "https://image.tmdb.org/t/p/w500"
